9a624de321514d2acbb7fef082af36e5b986384c
Florent Jaby <fjaby@octo.com>
2014-07-25 13:56:59 +0200

    emit change event when etag changes

--- test/test-100-etag.js
+++ test/test-100-etag.js
@@ -12,7 +12,7 @@ describe('the ETag watcher', function () {
   };
   var watchers;
   beforeEach(function () {
-    nock('http://google.com').head('/').reply(200, '', {ETag: 'google'});
+    nock('http://example.com').head('/').reply(200, '', {ETag: 'google'});
     watchers = [];
   })
   afterEach(function () {
@@ -24,14 +24,14 @@ describe('the ETag watcher', function () {
 
   it('should be a constructor', function () {
     var etag = require('..').etag;
-    var depends = new etag('http://google.com')
+    var depends = new etag('http://example.com')
     expect(depends).to.be.an('object')
     expect(depends.constructor).to.equal(etag)
   })
 
   it('should be a constructor even without new', function () {
     var etag = require('..').etag;
-    var depends = etag('http://google.com')
+    var depends = etag('http://example.com')
     expect(depends).to.be.an('object')
     expect(depends.constructor).to.equal(etag)
   })
@@ -49,22 +49,42 @@ describe('the ETag watcher', function () {
   });
 
   it('should make a HEAD request every X seconds according ot its timeout parameter', function (done) {
-    // here we expect to get 3 requests in 500 ms given that the timeout is 200ms
+    // here we expect to get 3 requests in 50 ms given that the timeout is 20ms
     var expectHead = nock('http://some.url')
                     .head('/resource')
                     .times(3)
                     .reply(200, "", {ETag: '888'})
 
     var depends = etag('http://some.url/resource', '888', {
-      interval: 200
+      interval: 20
     }); 
 
 
     setTimeout(function () {
       expectHead.done();
       done()
-    }, 510)
+    }, 51)
   })
+
+  it('should emit a change event when the ETag changes', function (done) {
+    var head1 = nock('http://some.url')
+      .head('/resource')
+      .reply(200, "", {ETag: '888'})
+    var head2 = nock('http://some.url')
+      .head('/resource')
+      .reply(200, "", {ETag: '999'})
+
+    var depends = etag('http://some.url/resource', '888', {interval: 20});
+    depends.on('change', function(info) {
+      head1.done();
+      head2.done();
+      expect(info).to.be.an('object');
+      expect(info).to.have.property('changeId');
+      expect(info.oldETag).to.equal('888');
+      expect(info.newETag).to.equal('999');
+      done();
+    });
+  });
 
   // 'change'
   // check(cb)
