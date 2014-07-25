428073f5a886d5b3a3693394688479aed6ead68c
Florent Jaby <fjaby@octo.com>
2014-07-25 14:25:14 +0200

    test errors are propagated

--- test/test-100-etag.js
+++ test/test-100-etag.js
@@ -1,5 +1,6 @@
 var nock = require('nock');
 var expect = require('chai').expect
+var trycatch = require('trycatch');
 var it = require('./utils').tcit
 
 describe('the ETag watcher', function () {
@@ -85,6 +86,46 @@ describe('the ETag watcher', function () {
       done();
     });
   });
+
+  it('should stop polling the URL once the change event has been fired', function (done) {
+    var head1 = nock('http://some.url')
+      .head('/resource')
+      .reply(200, "", {ETag: '888'})
+    var head2 = nock('http://some.url')
+      .head('/resource')
+      .reply(200, "", {ETag: '999'})
+
+    var depends = etag('http://some.url/resource', '888', {interval: 20});
+    depends.on('change', function(info) {
+      head1.done();
+      head2.done();
+
+      // wait 30 ms more, nock should not be called
+      var last = nock('http://some.url').head('/resource').reply(200, '', {ETag: '999'});
+      setTimeout(function () {
+        expect(last.isDone()).to.equal(false, 'The last mock should not have played');
+        expect(last.pendingMocks()).to.have.length(1, 'There should be one mock pending');
+        done();
+      }, 30);
+    });
+  });
+
+  it('should emit an error when a request emits and error', function (done) {
+    var isDone;
+    function myDone (err) {
+      if(isDone) return;
+      isDone = true;
+      done(err)
+    }
+    trycatch(function () {
+      nock('http://some.url').head('/resource').reply(200, '', {ETag: '888'});
+      var depends = etag('http://some.url/resource', '888', {interval: 10});
+      depends.on('error', function (err) {
+        expect(err).to.be.an.instanceof(Error);
+        myDone();
+      })
+    }, myDone);
+  })
 
   // 'change'
   // check(cb)
